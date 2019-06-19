84f7325561bb68b5c811e6bd9f60e4e1b3a52f82
Nnanyielugo <kz.onyegbule@gmail.com>
2019-06-19 10:45:35 +0100

    refactor helpers

--- src/picker/helpers.js
+++ src/picker/helpers.js
@@ -8,7 +8,8 @@
  * unchecks all other items to remove possible previously selected item(s),
  * returns the updated list
  */
-export function updateList(list, id) {
+export function updateList(_list, id) {
+  const list = JSON.parse(JSON.stringify(_list));
   if (!Array.isArray(list)) {
     throw new Error('invalid argument provided. Array expected');
   }
@@ -17,40 +18,61 @@ export function updateList(list, id) {
   if (value.checked != undefined && value.checked === false) {
     value.checked = true;
   }
-  list.map((item) => {
-    if (item.id !== id) {
-      item.checked = false; // eslint-disable-line no-param-reassign
-    }
-  });
+
+  const prevSelectedIndex = _list.findIndex(item => item.checked);
+  if (prevSelectedIndex !== -1) {
+    const previous = { ...list[prevSelectedIndex] };
+    previous.checked = false;
+  }
+
+  // list.map((item) => {
+  //   if (item.id !== id) {
+  //     item.checked = false; // eslint-disable-line no-param-reassign
+  //   }
+  // });
   return list;
 }
 
 export function updateListOnMount(_list, name, useValue = false) {
-  let value;
-  let list = [];
+  const list = JSON.parse(JSON.stringify(_list));
   if (useValue) {
-    value = _list.find(item => item.value === name);
+    const value = list.find(item => item.value === name);
     if (value) {
-      list = _list.map((item) => {
-        if (item.value === value.value) {
-          item.checked = true; // eslint-disable-line no-param-reassign
-        }
-        return item;
-      });
-      return list;
+      const checkedFromList = list.find(item => item.value === value.value);
+      if (checkedFromList) checkedFromList.checked = true;
     }
-  }
-  value = _list.find(item => item.name === name);
-  if (value) {
-    list = _list.map((item) => {
-      if (item.name === value.name) {
-        item.checked = true; // eslint-disable-line no-param-reassign
-      }
-      return item;
-    });
     return list;
   }
-  return _list;
+  const value = list.find(item => item.name === name);
+  if (value) {
+    const checkedFromList = list.find(item => item.name === value.name);
+    if (checkedFromList) checkedFromList.checked = true;
+  }
+  return list;
+
+  // if (useValue) {
+  //   value = _list.find(item => item.value === name);
+  //   if (value) {
+  //     list = _list.map((item) => {
+  //       if (item.value === value.value) {
+  //         item.checked = true; // eslint-disable-line no-param-reassign
+  //       }
+  //       return item;
+  //     });
+  //     return list;
+  //   }
+  // }
+  // value = _list.find(item => item.name === name);
+  // if (value) {
+  //   list = _list.map((item) => {
+  //     if (item.name === value.name) {
+  //       item.checked = true; // eslint-disable-line no-param-reassign
+  //     }
+  //     return item;
+  //   });
+  //   return list;
+  // }
+  // return list;
 }
 
 export function getSelectedFromName(list = [], name, useValue) {
