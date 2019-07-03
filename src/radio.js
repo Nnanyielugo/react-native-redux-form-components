0d2b0762cb48a92cf05c0c1e6e3fcf6832fa7635
Nnanyielugo <kz.onyegbule@gmail.com>
2019-07-03 12:32:03 +0100

    make component radio update on click, rather than when the component registers an update

--- src/radio.js
+++ src/radio.js
@@ -1,44 +1,82 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { View, TouchableOpacity } from 'react-native';
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 class Radio extends Component {
-  touchable = this.props.touchable
-
   static propTypes = {
-    marked: PropTypes.bool,
-    onPress: (prop, propName, component) => {
-      // eslint-disable-next-line eqeqeq
-      if (prop.touchable === true && prop[propName] == undefined) {
-        return new Error(`${propName} is marked as required if touchable is true in ${component}`);
-      }
-    },
     size: PropTypes.number,
     disabled: PropTypes.bool,
     color: PropTypes.string,
-    touchable: PropTypes.bool,
+    input: PropTypes.object,
+    initial: PropTypes.bool,
+    onClick: PropTypes.func,
+    toggle: PropTypes.bool,
   }
 
   static defaultProps = {
-    marked: true,
     size: 30,
     disabled: false,
     color: 'grey',
-    touchable: false,
+  }
+
+  state = {
+    marked: false,
+  }
+
+  componentDidMount() {
+    const { input, initial } = this.props;
+    this.isReduxForm = !!input;
+    this.setState({
+      marked: this.isReduxForm ? input.value : initial,
+    });
+  }
+
+  /* eslint-disable no-lonely-if, react/no-did-update-set-state */
+  componentDidUpdate(prevProps) {
+    const { input, initial } = this.props;
+    if (this.isReduxForm) {
+      if (prevProps.input.value !== input.value) {
+        this.setState({ marked: input.value });
+      }
+    } else {
+      if (prevProps.initial !== initial) {
+        this.setState({ marked: initial });
+      }
+    }
+  }
+  /* eslint-enable no-lonely-if react/no-did-update-set-state */
+
+  changeMarked = () => {
+    const { onClick, toggle } = this.props;
+    const { marked } = this.state;
+    if (toggle) {
+      this.setState(prevState => ({
+        ...prevState,
+        marked: !prevState.marked,
+      }));
+    }
+    if (this.isReduxForm) {
+      const { input: { onChange, value } } = this.props;
+      onChange(!value);
+    } else if (typeof onClick === 'function') {
+      onClick();
+    }
+
+    this.setState({ marked: !marked });
   }
 
   render() {
-    const {
-      marked, size, color, disabled, onPress, touchable,
-    } = this.props;
-    if (!touchable) {
-      return <Icon name={marked ? 'radiobox-marked' : 'radiobox-blank'} size={size} color={disabled ? 'gray' : color} />;
-    }
+    const { size, color, disabled } = this.props;
+    const { marked } = this.state;
     return (
-      <TouchableOpacity onPress={onPress}>
+      <TouchableOpacity onPress={this.changeMarked}>
         <View>
-          <Icon name={marked ? 'radiobox-marked' : 'radiobox-blank'} size={size} color={disabled ? 'gray' : color} />
+          <Icon
+            name={marked ? 'ios-radio-button-on' : 'ios-radio-button-off'}
+            size={size}
+            color={disabled ? 'whitesmoke' : color}
+          />
         </View>
       </TouchableOpacity>
     );
